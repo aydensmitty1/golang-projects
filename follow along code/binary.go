4929d948891b3b1567456081e6ca45141a49f7c0
aydensmitty1 <ayden.smith@hpe.com>
2017-08-11 10:53:34 -0600

    fixed binary

--- follow along code/binary.go
+++ follow along code/binary.go
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
 package main
 
 import "fmt"
 
 func main() {
-    fmt.Printf("%d -%b \n", 42, 42)
-  }
-=======
-package main
-
-import "fmt"
-
-func main() {
-    fmt.Printf("%d -%b \n", 42, 42)
-  }
->>>>>>> 0fbe243d033a2f482bc29ec3e3be2e16aeeb2a21
+	fmt.Printf("%d -%b \n", 42, 42)
+}
